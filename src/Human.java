7cb9681b2e5b96fa168825db97f2a3aee99100c9
AliJafarli <somagni4icent@gmail.com>
2024-10-23 19:14:56 +0400

    a

--- src/Human.java
+++ src/Human.java
@@ -3,6 +3,18 @@ public class Human {
     public String surname;
     public int year;
     public int iq;
+    public Pet pet;
+
+    String[][] schedule;
 
 
+    public String greetPet(String nickname) {
+        return nickname;
+
+    }
+
+    public void describePet(){
+        String slyLVL = pet.trickLevel > 50 ? "very sly":"almost not sly";
+        System.out.println("I have a " + pet.species + ", he is "+ pet.age +"years old, he is " + slyLVL);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        Human human = new Human();
+
+        human.schedule = new String[][]{
+                {"Monday", "Gym", "Reading"},
+                {"Tuesday", "Running", "Cooking"},
+                {"Wednesday", "Swimming", "Painting"},
+                {"Thursday", "Yoga", "Gaming"},
+                {"Friday", "Cycling", "Photography"},
+                {"Saturday", "Hiking", "Gardening"},
+                {"Sunday", "Rest", "Family Time"}
+        };
+
     }
 }
